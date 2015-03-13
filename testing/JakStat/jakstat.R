14b157ddf4ecc9a545f39ee6ee57af04a7f309a1
Daniel Kaschek <daniel.kaschek@physik.uni-freiburg.de>
2015-03-13 11:06:22 +0100

    FIX: JakStat: Update of the JakState test model

--- testing/JakStat/jakstat.R
+++ testing/JakStat/jakstat.R
@@ -22,7 +22,7 @@
   fixedStates <- c("pSTAT", "pSTATdimer", "npSTATdimer", "nSTAT1", "nSTAT2", "nSTAT3", "nSTAT4", "nSTAT5")
 # Add observables to ODE  
   eq <- addObservable(observables, eq)
-  model <- generateModel(eq, einspline=TRUE, forcings = forcings, fixed = fixedStates, nGridpoints = 101, jacobian = "inz.lsodes")
+  model <- generateModel(eq, einspline=FALSE, forcings = forcings, fixed = fixedStates, nGridpoints = 101, jacobian = "jacvec.lsodes")
   
 ## Get data --------------------------------------------------
   
@@ -83,30 +83,4 @@
 # Evaluate model at best-fitting parameter values and plot
   prediction <- x(times, p(myfit$argument))
   plotCombined(list(states=prediction, input=long2wide(mydataReceptor)), list(states=mydata, input=mydataReceptor))
-  
-## Multiple fits ------------------------------------------
-
-# Fit repeatedly for different initial parameter values
-# Return data.frame with the final objective value and 
-# the best-fitting parameter vector
-  fitlist <- mclapply(1:100, function(i) {
-    
-    mypini <- rnorm(length(outerpars), pini, 5)
-    names(mypini) <- outerpars
-    myfit <- trust(myfn, mypini, rinit=1, rmax=10, iterlim=100)
-    out <- data.frame(chisquare = myfit$value, as.data.frame(as.list(myfit$argument)))
-    
-  }, mc.preschedule=FALSE, mc.cores=4)
-# Sort the data.frame by increasing objective values and plot  
-  fitlist <- do.call(rbind, fitlist[sapply(fitlist, class) == "data.frame"])
-  fitlist <- fitlist[order(fitlist$chisquare),]
-  qplot(y=fitlist[,1])
-
-## Compute profiles of best fit -----------------------------
-  
-  bestfit <- unlist(fitlist[1,-1])
-  proflist.approx <- do.call(c, mclapply(names(bestfit), function(n) profile.trust(myfn, bestfit, n, limits=c(-3, 3)), mc.cores=4))
-  proflist.exact  <- do.call(c, mclapply(names(bestfit), function(n) profile.trust(myfn, bestfit, n, limits=c(-3, 3), algoControl = list(reoptimize = TRUE)), mc.cores=4))
-  
-  plotProfile(proflist.approx, proflist.exact)
   
