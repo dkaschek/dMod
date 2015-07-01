3f48de0912279a72782589b96852926f1e84e503
Wolfgang Mader <Wolfgang.Mader@fdm.uni-freiburg.de>
2015-07-01 22:44:46 +0200

    TEST: Unit test for strpad.

---
+++ inst/tests/test-str_padding.R
@@ -0,0 +1,36 @@
+context("String padding")
+
+
+# Set up environment
+src <- "1234567890"
+
+paddingBlank <- paste0(rep(" ", 10), collapse = "")
+paddingDots <- paste0(rep(".", 10), collapse = "")
+
+
+test_that("string padding with default filling works properly", {
+  expect_that(strpad(src, 20), equals(paste0(src, paddingBlank)))
+  expect_that(strpad(src, 20, where = "right"), equals(paste0(src, paddingBlank)))
+  expect_that(strpad(src, 20, where = "left"), equals(paste0(paddingBlank, src)))
+})
+  
+test_that("string padding with custom filling works properly", {
+  expect_that(strpad(src, 20, padding = '.'), equals(paste0(src, paddingDots)))
+  expect_that(strpad(src, 20, padding = '.', where = "right"), equals(paste0(src, paddingDots)))
+  expect_that(strpad(src, 20, padding = '.', where = "left"), equals(paste0(paddingDots, src)))
+})
+
+test_that("padding with auto elide works properly", {
+  expect_that(strpad(src, 6, autoelide = TRUE), equals("123..."))
+  expect_that(strpad(src, 6, where = "right", autoelide = TRUE), equals("123..."))
+  expect_that(strpad(src, 6, where = "left", autoelide = TRUE), equals("...890"))
+})
+
+test_that("error are reported properly", {
+  src <- "Wrong class"
+  class(src) <- "MyClass"
+  expect_that(strpad(src, 20), gives_warning())
+  expect_that(strpad(src, 20), equals(NULL))
+              
+  expect_that(strpad("1", padding = ".."), throws_error())
+})
