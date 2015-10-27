144cb68e1bdc9b4ec62772565ba2286a355cd646
Wolfgang Mader <Wolfgang.Mader@fdm.uni-freiburg.de>
2015-10-27 11:26:24 +0100

    FEATURE: Implement constuctor for parlist.

--- R/classes.R
+++ R/classes.R
@@ -1,38 +1,38 @@
 ## Equation classes -------------------------------------------------------
 
 #' Generate equation vector object
-#' 
+#'
 #' @description The eqnvec object stores explicit algebraic equations, like the
 #' right-hand sides of an ODE, observation functions or parameter transformations
 #' as named character vectors.
-#' @param equations (named) character of symbolic mathematical expressions, 
+#' @param equations (named) character of symbolic mathematical expressions,
 #' the right-hand sides of the equations
 #' @param names character, the left-hand sides of the equation
 #' @return object of class \code{eqnvec}, basically a named character.
 #' @export
 eqnvec <- function(equations = NULL, names = NULL) {
-  
+
   if (is.null(equations)) return(c())
-  
+
   if (is.null(names)) names <- names(equations)
   if (is.null(names)) stop("equations need names")
   if (length(names) != length(equations)) stop("Length of names and equations do not coincide")
   try.parse <- try(parse(text = equations), silent = TRUE)
   if (inherits(try.parse, "try-error")) stop("equations cannot be parsed")
-  
+
   out <- structure(equations, names = names)
   class(out) <- "eqnvec"
-  
+
   return(out)
-  
+
 }
 
 #' Generate eqnlist object
-#' 
+#'
 #' @description The eqnlist object stores an ODE as a list of stoichiometric matrix,
 #' rate expressions, state names and compartment volumes.
 #' @export
-#' @param smatrix Matrix of class numeric. The stoichiometric matrix, 
+#' @param smatrix Matrix of class numeric. The stoichiometric matrix,
 #' one row per reaction/process and one column per state.
 #' @param states Character vector. Names of the states.
 #' @param rates Character vector. The rate expressions.
@@ -42,7 +42,7 @@ eqnvec <- function(equations = NULL, names = NULL) {
 #' @param description Character vector. Description of the single processes.
 #' @return An object of class \code{eqnlist}, basically a list.
 eqnlist <- function(smatrix = NULL, states = colnames(smatrix), rates = NULL, volumes = NULL, description = NULL) {
-  
+
   # Dimension checks and preparations for non-empty argument list.
   if (all(!is.null(c(smatrix, states, rates)))) {
     #Dimension checks
@@ -51,7 +51,7 @@ eqnlist <- function(smatrix = NULL, states = colnames(smatrix), rates = NULL, vo
     l3 <- length(rates)
     if (l2 != d1[2]) stop("Number of states does not coincide with number of columns of stoichiometric matrix")
     if (l3 != d1[1]) stop("Number of rates does not coincide with number of rows of stoichiometric matrix")
-    
+
     # Prepare variables
     smatrix <- as.matrix(smatrix)
     colnames(smatrix) <- states
@@ -59,14 +59,14 @@ eqnlist <- function(smatrix = NULL, states = colnames(smatrix), rates = NULL, vo
       description <- 1:nrow(smatrix)
     }
   }
-  
+
   out <- list(smatrix = smatrix,
               states = as.character(states),
               rates = as.character(rates),
               volumes = volumes,
               description = as.character(description))
   class(out) <- "eqnlist"
-  
+
   return(out)
 }
 
@@ -90,38 +90,37 @@ eqnlist <- function(smatrix = NULL, states = colnames(smatrix), rates = NULL, vo
 #' different names. Inherits from data.frame.
 #' @export
 parframe <- function(x = NULL, parameters = colnames(x), metanames = NULL, obj.attributes = NULL) {
-  
+
   if (!is.null(x)) {
     rownames(x) <- NULL
     out <- as.data.frame(x)
   } else {
     out <- data.frame()
   }
-  
+
   attr(out, "parameters") <- parameters
   attr(out, "metanames") <- metanames
   attr(out, "obj.attributes") <- obj.attributes
   class(out) <- c("parframe", "data.frame")
-  
+
   return(out)
-  
+
 }
 
 #' @export
-parlist <- function(mylist, myframe) {
-  
-  # Wie erzeugt man myframe aus mylist?
-  
-  out <- mylist
-  attr(out, "parframe") <- myframe
-  
-  return(out)
-  
+parlist <- function(x = NULL) {
+  if (is.null(x)) {
+    return(NULL)
+  } else {
+    class(x) <- c("parlist", "list")
+    return(x)
+  }
 }
 
 
+
 #' Parameter vector
-#' 
+#'
 #' @description A parameter vector is a named numeric vector (the parameter values)
 #' together with an "deriv" attribute (the Jacobian of a parameter transformation by which
 #' the parameter vector was generated).
@@ -130,23 +129,24 @@ parlist <- function(mylist, myframe) {
 #' @param deriv matrix with rownames according to \code{mynames} and colnames
 #' according to the names of the parameter by which \code{p} was generated.
 #' @return An object of class \code{parvec}, i.e. a named numeric vector with attribute "deriv".
+#'
 #' @export
 parvec <- function(p, mynames = names(p), deriv = NULL) {
-  
+
   out <- as.numeric(p)
   names(out) <- mynames
   attr(out, "deriv") <- deriv
   class(out) <- c("parvec", "numeric")
-  
+
   return(out)
-  
+
 }
 
 
 ## Prediction classes ----------------------------------------------------
 
 #' Prediction function
-#' 
+#'
 #' @description A prediction function is a function \code{x(times, pars, fixed, deriv, ...)}
 #' which returns a list of model predictions. Each entry of the list is a \link{prdframe}
 #' as being produced by a low-level prediction function, see e.g. \link{Xs}. The different entries
@@ -163,12 +163,12 @@ parvec <- function(p, mynames = names(p), deriv = NULL) {
 #' which returns a \link{prdlist}.
 #' @export
 prdfn <- function(..., pouter = NULL, conditions = "1") {
-  
+
   force(pouter)
   force(conditions)
-  
+
   myexpr <- as.expression(substitute(...))
-  
+
   # Dummy constructor
   is.nullexpr <- deparse(myexpr)[1] == "expression(NULL)"
   if (is.nullexpr) myexpr <- expression({
@@ -177,13 +177,13 @@ prdfn <- function(..., pouter = NULL, conditions = "1") {
     myderivs <- out
     prdframe(out, myderivs, names(pars))
   })
-  
+
   # Prediction function
   myfn <- function(times, pars = pouter, fixed = NULL, deriv = TRUE, ...){
-    
+
     prdlist(
       lapply(conditions, function(condition) {
-        eval(myexpr)    
+        eval(myexpr)
       }),
       conditions
     )
@@ -192,11 +192,11 @@ prdfn <- function(..., pouter = NULL, conditions = "1") {
   attr(myfn, "pouter") <- pouter
   attr(myfn, "parameters") <- names(pouter)
   return(myfn)
-  
+
 }
 
 #' Prediction frame
-#' 
+#'
 #' @description A prediction frame is used to store a model description in a matrix. The columns
 #' of the matrix are "time" and one column per state. The prediction frame has attributes "deriv",
 #' the matrix of sensitivities with respect to "outer parameters" (see \link{P}), an attribute
@@ -210,20 +210,20 @@ prdfn <- function(..., pouter = NULL, conditions = "1") {
 #' @return Object of class \code{prdframe}, i.e. a matrix with other matrices and vectors as attributes.
 #' @export
 prdframe <- function(prediction = NULL, deriv = NULL, sensitivities = NULL, parameters = NULL) {
-  
+
   out <- if (!is.null(prediction)) as.matrix(prediction) else matrix()
-  
+
   attr(out, "deriv") <- deriv
   attr(out, "sensitivities") <- sensitivities
   attr(out, "parameters") <- parameters
   class(out) <- c("prdframe", "matrix")
-  
+
   return(out)
-  
+
 }
 
 #' Prediction list
-#' 
+#'
 #' @description A description list is used to store a list of model predictions
 #' from different prediction functions or the same prediction function with different
 #' parameter specifications. Each entry of the list is a \link{prdframe}.
@@ -232,17 +232,17 @@ prdframe <- function(prediction = NULL, deriv = NULL, sensitivities = NULL, para
 #' conditions.
 #' @export
 prdlist <- function(mylist = NULL, mynames = names(mylist)) {
-  
+
   if (is.null(mylist)) mylist <- list()
-  
+
   if (length(mynames) != length(mylist)) stop("names argument has wrong length")
-  
+
   ## Prepare output
   names(mylist) <- mynames
   class(mylist) <- c("prdlist", "list")
-  
+
   return(mylist)
-  
+
 }
 
 
@@ -250,40 +250,40 @@ prdlist <- function(mylist = NULL, mynames = names(mylist)) {
 ## Data classes ----------------------------------------------------------------
 
 #' Generate a datalist object
-#' 
+#'
 #' @description The datalist object stores time-course data in a list of data.frames.
 #' The names of the list serve as identifiers, e.g. of an experimental condition, etc.
-#' @param mylist list of data.frame, each data.frame is expected to have columns 
+#' @param mylist list of data.frame, each data.frame is expected to have columns
 #' "name" (factor or character),
 #' "time" (numeric), "value" (numeric) and "sigma" (numeric).
 #' @param mynames character vector of the length of mylist.
 #' @return Object of class \code{datalist}.
 #' @export
 datalist <- function(mylist, mynames = names(mylist)) {
-  
+
   ## Check properties
   is.data.frame <- sapply(mylist, class) == "data.frame"
   if (!all(is.data.frame)) stop("list of data.frame expected")
-  
+
   correct.names <- c("name", "time", "value", "sigma")
   have.correct.names <- sapply(mylist, function(d) all(colnames(d) %in% correct.names))
   if (!all(have.correct.names)) stop(paste("data.frames should have names:", correct.names, collapse = " "))
-  
+
   if (length(mynames) != length(mylist)) stop("names argument has wrong length")
-  
+
   ## Prepare output
   names(mylist) <- mynames
   class(mylist) <- c("datalist", "list")
-  
+
   return(mylist)
-  
+
 }
 
 
 ## Objective classes ---------------------------------------------------------
 
 #' Objective function
-#' 
+#'
 #' @description An objective function is a function \code{obj(pouter, fixed, deriv, ...)} which
 #' returns an \link{objlist}. This function is supposed to be used in an optimizer like \code{trust}
 #' from the \code{trust} package.
@@ -300,44 +300,44 @@ datalist <- function(mylist, mynames = names(mylist)) {
 #' which returns an \link{objlist}.
 #' @export
 objfn <- function(..., data, x, pouter = NULL, conditions = names(data)) {
-  
+
   force(data)
   force(x)
   force(pouter)
   force(conditions)
-  
+
   myexpr <- as.expression(substitute(...))
   timesD <- sort(c(0, unique(do.call(c, lapply(data, function(d) d$time)))))
-  
+
   myfn <- function(pouter = pouter, fixed = NULL, deriv=TRUE, ...){
-    
+
     prediction <- x(times = timesD, pars = pouter, fixed = fixed, deriv = deriv)
-    
+
     # Apply res() and wrss() to compute residuals and the weighted residual sum of squares
     out.data <- lapply(conditions, function(cn) wrss(res(data[[cn]], prediction[[cn]])))
     out.data <- Reduce("+", out.data)
-    
+
     # Evaluate user-defined contributions, e.g. priors
     out.user <- eval(myexpr)
     #attributes.user <- attributes(out.user)
     #attributes.user <- attributes.user[!names(attributes.user) %in% c("names", "class")]
-    
+
     # Combine contributions and attach attributes
     out <- out.data + out.user
     attr(out, "data") <- out.data$value
     attr(out, "user") <- out.user$value
     return(out)
-    
-      
+
+
   }
   class(myfn) <- "objfn"
   return(myfn)
-  
+
 }
 
 
 #' Generate objective list
-#' 
+#'
 #' @description An objective list contains an objective value, a gradient, and a Hessian matrix
 #' @param value numeric of length 1
 #' @param gradient named numeric
@@ -345,36 +345,36 @@ objfn <- function(..., data, x, pouter = NULL, conditions = names(data)) {
 #' @return Object of class \code{objlist}
 #' @export
 objlist <- function(value, gradient, hessian) {
-  
+
   out <- list(value = value, gradient = gradient, hessian = hessian)
   class(out) <- "objlist"
   return(out)
-  
+
 }
 
 #' Objective frame
-#' 
+#'
 #' @description An objective frame is supposed to store the residuals of a model prediction
-#' with respect to a data frame. 
+#' with respect to a data frame.
 #' @param mydata data.frame as being generated by \link{res}.
 #' @param deriv matrix of the derivatives of the residuals with respect to parameters.
 #' @return An object of class \code{objframe}, i.e. a data frame with attribute "deriv".
 #' @export
 objframe <- function(mydata, deriv = NULL) {
-  
+
   # Check column names
   mydata <- as.data.frame(mydata)
-  correct.names <- c("time", "name", "value", "prediction", 
+  correct.names <- c("time", "name", "value", "prediction",
                      "sigma", "residual", "weighted.residual")
-  
+
   ok <- all(correct.names %in% names(mydata))
   if (!ok) stop("mydata does not have required names")
-  
+
   out <- mydata[, correct.names]
   attr(out, "deriv") <- deriv
   class(out) <- "objframe"
-  
+
   return(out)
-  
-  
+
+
 }
