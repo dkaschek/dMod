3490d30e4411806e4b7cd5fcb13b2a67d20e38a9
Daniel Kaschek <daniel.kaschek@physik.uni-freiburg.de>
2016-02-20 09:12:23 +0100

    FEATURE: Subsetting parframes with "[" operator.
    ATTENTION: The behavior differs from that of "[.data.frame" and is more matrix-like.

--- R/parClass.R
+++ R/parClass.R
@@ -90,15 +90,37 @@ as.parframe.parlist <- function(x, sort.by = "value") {
                             as.data.frame(as.list(fit$argument))
                           )
                         }, fit = x[m_idx], idx = m_idx, SIMPLIFY = FALSE))
-  m_parframe <- parframe(m_parframe, parameters = names(x[[m_idx[1]]]$parinit), metanames = m_metanames)
   # Sort by value
   m_parframe <- m_parframe[order(m_parframe[sort.by]),]
   
-  return(m_parframe)
+  parframe(m_parframe, parameters = names(x[[m_idx[1]]]$parinit), metanames = m_metanames)
+  
+  
 }
 
 
 ## Methods for the class parframe -----------------------------------------------
+
+#' @export
+"[.parframe" <- function(x, i = NULL, j = NULL){
+  
+  metanames <- attr(x, "metanames")
+  obj.attributes <- attr(x, "obj.attributes")
+  parameters <- attr(x, "parameters")
+ 
+  out <- as.data.frame(x)
+  #out <- as.data.frame(unclass(x))
+  if (!is.null(i)) out <- out[i, ]
+  if (!is.null(j)) out <- out[, j]
+  
+  metanames <- intersect(metanames, colnames(out))
+  obj.attributes <- intersect(obj.attributes, colnames(out))
+  parameters <- intersect(parameters, colnames(out))
+  
+  parframe(out, parameters = parameters, metanames = metanames, obj.attributes = obj.attributes)
+  
+}
+
 
 #' @export
 subset.parframe <- function(x, ...) {
