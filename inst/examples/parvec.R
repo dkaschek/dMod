8721a5164aa696b5f7603637d8db20baec08b526
Daniel Kaschek <daniel.kaschek@physik.uni-freiburg.de>
2016-03-19 00:21:24 +0100

    FEATURE: Vignette.

--- inst/examples/parvec.R
+++ inst/examples/parvec.R
@@ -1,7 +1,7 @@
 # Generate a parameter vector
 v <- parvec(a = 2, b = 3)
 print(v)
-print(derivatives(v))
+print(getDerivs(v))
 
 # Parameter vector from a named numeric
 M <- matrix(c(1, 1, 0, 1), 
@@ -10,20 +10,20 @@ M <- matrix(c(1, 1, 0, 1),
     )
 v <- as.parvec(x = c(a = 2, b = 3), deriv = M)
 print(v)
-print(derivatives(v))
+print(getDerivs(v))
 
 # Subsetting of parameter vectors
 # Case 1: Dependencies in the Jacobian are maintained
 w <- v[1]
 print(w)
-print(derivatives(w))
+print(getDerivs(w))
 
 # Case 2: Dependencies are dropped
 w <- v[1, drop = TRUE]
 print(w)
-print(derivatives(w))
+print(getDerivs(w))
 
 # Concatenating parameter vectors
 w <- parvec(c = 4, d = 5)
 print(c(v, w))
-print(derivatives(c(v, w)))
+print(getDerivs(c(v, w)))
