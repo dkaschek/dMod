05988b8eb518ee263072f313ac4f9083df34d085
Daniel Kaschek <daniel.kaschek@physik.uni-freiburg.de>
2016-05-30 11:27:41 +0200

    BUGFIX: subset.eqnlist() would not work on bi-molecular reactions.

--- inst/examples/equations.R
+++ inst/examples/equations.R
@@ -1,27 +1,46 @@
-## Generate another equation list
-eq <- eqnlist()
-eq <- addReaction(eq, "A", "pA", "act_A * A * stimulus", "Phosphorylation of A")
-eq <- addReaction(eq, "pA", "A", "deact_A * pA", "Deposphorylation of pA")
-eq <- addReaction(eq, "B", "pB", "act_B * B * pA", "Phosphorylation of B")
-eq <- addReaction(eq, "pB", "B", "deact_B * pB", "Deposphorylation of pB")
 
-## Extract data.frame of reactions
-getReactions(eq)
-
-## Get conserved quantities
-conservedQuantities(eq$smatrix)
-
-## Get fluxes
-getFluxes(eq)
-
-## Subsetting of equation list
-subset(eq, "pB" %in% Product)
-subset(eq, grepl("Phosphorylation", Description))
-
-## Time derivatives of observables
-observables <- eqnvec(pA_obs = "s1*pA", tA_obs = "s2*(A + pA)")
-dobs <- dot(observables, eq)
-
-## Combined equation vector for ODE and observables
-f <- c(as.eqnvec(eq), dobs)
-print(f)
+  # Write your example here. You can also add more Start..End blocks if needed.
+  # Please mask all output such as print() with the special tag
+  #    
+  # such that the test is not littered. Statements guarded by  are enabled
+  # in the example file which is extracted from this test file. To extract the
+  # example run
+  #    extractExamples()
+  # on the R command line.
+    
+    ## Generate another equation list
+    eq <- eqnlist()
+    eq <- addReaction(eq, "A", "pA", "act_A * A * stimulus", "Phosphorylation of A")
+    eq <- addReaction(eq, "pA", "A", "deact_A * pA", "Deposphorylation of pA")
+    eq <- addReaction(eq, "2*pA", "pA_pA", "form_complex_pA * pA^2", "Complex formation of pA")
+    eq <- addReaction(eq, "B", "pB", "act_B * B * pA_pA", "Phosphorylation of B")
+    eq <- addReaction(eq, "pB", "B", "deact_B * pB", "Deposphorylation of pB")
+    
+    ## Extract data.frame of reactions
+    reactions <- getReactions(eq)
+     print(reactions)
+    
+    ## Get conserved quantities
+    cq <- conservedQuantities(eq$smatrix)
+     print(cq)
+    
+    ## Get fluxes
+    fluxes <- getFluxes(eq)
+     print(fluxes)
+    
+    ## Subsetting of equation list
+    subeq1 <- subset(eq, "pB" %in% Product)
+     print(subeq)
+    subeq2 <- subset(eq, grepl("not_available", Description))
+     print(subeq)
+    
+    ## Time derivatives of observables
+    observables <- eqnvec(pA_obs = "s1*pA", tA_obs = "s2*(A + pA)")
+    dobs <- dot(observables, eq)
+    
+    ## Combined equation vector for ODE and observables
+    f <- c(as.eqnvec(eq), dobs)
+     print(f)
+    
+    
+  
