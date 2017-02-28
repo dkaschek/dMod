27bf3f159357184e7a0e326e84273d3fb8e65e39
Daniel Kaschek <daniel.kaschek@physik.uni-freiburg.de>
2017-02-28 10:14:44 +0100

    FIX: Update of the error estimation example

--- inst/examples/errmodel.R
+++ inst/examples/errmodel.R
@@ -1,47 +1,54 @@
 
 library(dMod)
-library(deSolve)
 
+# Set up reactions
 f <- NULL
 f <- addReaction(f, "A", "B", "k1*A", "Production of B")
 f <- addReaction(f, "B", "C", "k2*B", "Production of C")
 
+# Define observables and error model
 observables <- eqnvec(B_obs = "B + off_B")
-errors <- eqnvec(B_obs = "sigma_rel*B_obs + sigma_abs")
+errors <- eqnvec(B_obs = "sqrt((sigma_rel*B_obs)^2 + sigma_abs^2)")
 
+# Generate dMod objects
 model <- odemodel(f)
-p <- P(
-  trafo = eqnvec(
-    A = "1", B = "0", C = "0", k1 = "exp(log_k1)", k2 = "exp(log_k2)", # dynamic parameters
-    off_B = "exp(log_off_B)", # observation parameters
-    sigma_rel = "exp(log_sigma_rel)", # error parameters
-    sigma_abs = "exp(log_sigma_abs)" # error parameters
-  ),
-  condition = "C1"
-)
-x <- Xs(model)
-g <- Y(observables, f)
-err <- Y(errors, c(observables, as.eqnvec(f)), states = names(observables),  attach.input = FALSE, compile = TRUE, modelname = "err")
+x     <- Xs(model)
+g     <- Y(observables, f, 
+           compile = TRUE, modelname = "obsfn")
+e     <- Y(errors, c(observables, as.eqnvec(f)), states = names(observables), attach.input = FALSE,
+           compile = TRUE, modelname = "errfn")
+
+
+# Generate parameter transformation
+innerpars <- getParameters(model, g, e)
+trafo <- repar("x~x", x = innerpars)
+trafo <- repar("x~1", x = "A", trafo)
+trafo <- repar("x~0", x = c("B", "C"), trafo)
+trafo <- repar("x~exp(x)", x = innerpars, trafo)
+
+p <- P(trafo, condition = "C1")
+
 
 ## Simulate data
-ptrue <- c(log_k1 = -1, log_k2 = -2, log_off_B = -1, log_sigma_rel = log(0.1), log_sigma_abs = log(.1))
-times <- 0:20
+ptrue <- c(k1 = -2, k2 = -3, off_B = -3, sigma_rel = log(.1), sigma_abs = log(.1))
+times <- seq(0, 50, 1)
 prediction <- (g*x*p)(times, ptrue)
-datasheet <- subset(wide2long(prediction), name == "B_obs")
-datasheet$sigma <- 0.1*datasheet$value
-datasheet$value <- datasheet$value + rnorm(length(datasheet$value), 0, datasheet$sigma)
+datasheet <- subset(as.data.frame(prediction, errfn = e), name == "B_obs")
+datasheet$value <- datasheet$value + rnorm(length(datasheet$value), sd = datasheet$sigma)
 data <- as.datalist(datasheet)
 
 ## Fit data with error model
-myfit <- trust(normL2(data, g*x*p, err), ptrue + rnorm(length(ptrue), 0, 1), rinit = 1, rmax = 10)
-profiles <- profile(normL2(data, g*x*p, err) + constraintL2(myfit$argument, 10), myfit$argument, names(myfit$argument), limits = c(-5, 5), cores = 4)
+myfit <- trust(normL2(data, g*x*p, e), ptrue, rinit = 1, rmax = 10)
+profiles <- profile(normL2(data, g*x*p, e) + constraintL2(myfit$argument, 10), 
+                    myfit$argument, names(myfit$argument), limits = c(-5, 5), cores = 4)
+plotProfile(profiles)
+
 
 ## Plotting
-out <- ggdata_fn(g*x*p, err, data, times = seq(0, 20, len = 100), pars = myfit$argument)
-ggplot(out$prediction, aes(x = time, y = value, ymin = value-sigma, ymax = value+sigma, 
-                           group = condition, color = condition, fill = condition)) +
+out <- as.data.frame((g*x*p)(times = seq(0, 50, len = 100), pars = myfit$argument), errfn = e)
+ggplot(out, aes(x = time, y = value, ymin = value-sigma, ymax = value+sigma, color = condition, fill = condition)) +
   facet_wrap(~name, scales = "free") +
   geom_line() + geom_ribbon(alpha = .2, lty = 0) + 
-  geom_point(data = out$data) + geom_errorbar(data = out$data, width = 0) +
+  geom_point(data = as.data.frame(data)) +
   theme_dMod() + scale_color_dMod() + scale_fill_dMod()
 
