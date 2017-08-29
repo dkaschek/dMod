5aa25703563ce3b0d7d0d674c7257f6c8140ad23
Daniel Kaschek <daniel.kaschek@physik.uni-freiburg.de>
2017-08-29 16:07:12 +0200

    Add global variables

--- R/dMod.R
+++ R/dMod.R
@@ -2,4 +2,4 @@ globalVariables(c("value", "sigma", "condition", "x", "y", "name", "proflist", "
                   "sigmaLS", "cbLower95", "cbUpper95", "cbLower68", "cbUpper68",
                   "iteration", "idx", "is.zero", "index", "converged", "iterations",
                   ".runbgOutput", "terminal", "parvalue", "dataErrorModel",
-                  "Rate"))
+                  "Rate", "weighted.residual"))
