270d81d7c4a268b70f027b9d49975b310d691261
Daniel Kaschek <daniel.kaschek@physik.uni-freiburg.de>
2017-08-29 16:27:15 +0200

    Remove all files that might conflict with branch master_precursor

--- R/dMod.R
+++
@@ -1,5 +0,0 @@
-globalVariables(c("value", "sigma", "condition", "x", "y", "name", "proflist", "delta",
-                  "sigmaLS", "cbLower95", "cbUpper95", "cbLower68", "cbUpper68",
-                  "iteration", "idx", "is.zero", "index", "converged", "iterations",
-                  ".runbgOutput", "terminal", "parvalue", "dataErrorModel",
-                  "Rate"))
--- inst/examples/eqnvec.R
+++
@@ -1,4 +0,0 @@
-v <- eqnvec(y = "2*x + offset")
-print(v)
-is.eqnvec(v)
-
