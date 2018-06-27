5773530e03afe50e2742a4a7ddcb3ee11bf33634
Mirjam Fehling-Kaschek <mirjam.fehling@physik.uni-freiburg.de>
2018-06-27 15:36:00 +0200

    add CCD4 example

---
+++ inst/examples/example_CCD4/analysis.R
@@ -0,0 +1,210 @@
+# publised in https://www.ncbi.nlm.nih.gov/pubmed/27811075
+# simple model, sort of A -> B -> C
+# no error model fitting
+
+## Model Definition ------------------------------------------------------
+
+# Read in model csv
+reactionlist <- read.csv("model_csv/model.csv") 
+
+f <- as.eqnlist(reactionlist)
+
+# Define new observables based on ODE states
+species <- f$states
+
+observables <- c(
+  obcar = "bcar",
+  obcry = "bcry",
+  obio = "bio",
+  ob10 = "b10",
+  oohb10 = "ohb10",
+  ozea = "zea"
+)
+
+# List of fixed parameters which are known beforehand
+fixed <- NULL
+forcings <- NULL 
+
+# Add observable ODEs to the original ODEs or use an observation function
+g <- Y(observables, as.eqnvec(f), compile = TRUE,modelname = "obs", attach.input = FALSE)
+
+# Generate the model C files, compile them and return a list with func and extended.
+cat("now compile the model", do.compile,"\n")
+model0 <- odemodel(as.eqnvec(f), fixed = fixed, forcings = NULL, jacobian = "inz.lsodes", compile = do.compile,modelname = "ccd4")
+
+## Data ----------------------------------------------------------------------
+
+# Data was preprocessed using an error model fit
+# datasheet <- subset(read.table("data/data_mark_out.csv",sep=",",header=TRUE), time >0 ) # needs columns condition, name, time, value, sigma, time = 0 were removed due to experimental problems
+# dMod::fitErrorModel(datasheet, factors = c("name"), plotting = FALSE, errorModel = "(exp(s0) + x^2*exp(srel))", par = c(s0 = -6, srel = -4))$sigma
+
+# Final data used for the publication (class: datalist):
+data <- readRDS("datalist.Rds")
+conditions <- getConditions(data)
+## Parameter Transformations -------------------------------------------
+
+# Define inner parameters (parameters occurring in the equations except forcings)
+# Add names(observables) if addObservables(observables, f) is used
+innerpars <- getSymbols(c(as.character(as.eqnvec(f)), names(as.eqnvec(f)), as.character(observables)), exclude=c(forcings, "time"))
+names(innerpars) <- innerpars
+
+# Define additional parameter constraints, e.g. steady-state conditions
+# Parameters (left-hand side) are replaced in the right-hand side of consecutive lines by resolveRecurrence() 
+# setting the initial value to zero for most states
+constraints <- c(
+  b10 = 0,
+  bio = 0,
+  ohb10 = 0,
+  ohbio = 0,
+  zea = 0
+)
+constraints <- resolveRecurrence(constraints)
+
+# Build up a parameter transformation (constraints, log-transform, etc.)
+# Start with replacing initial value parameters of the observables
+trafo <- replaceSymbols(names(observables), observables, innerpars)
+# Then employ the other parameter constraints
+trafo <- replaceSymbols(names(constraints), constraints, trafo)
+# Then do a log-transform of all parameters (if defined as positive numbers)
+parsT <- unique(c(getSymbols(constraints), innerpars))
+trafo <- replaceSymbols(parsT, paste0("exp(log", parsT, ")"), trafo)
+
+parameters <- innerpars[!(innerpars %in% species)]
+
+offsets <- paste0("log",parameters[grepl("off", parameters) & !(grepl("zea", parameters))& !(grepl("bcry", parameters))])
+
+## Specify different conditions -----------------------------------------------------
+# Set condition-specific parameter transformations
+# Mostly the conditions differ by the set of initial values for the states (different substrates provided for each condition)
+trafoLTot <- lapply(conditions, function(con){ 
+  trafo
+  if(grepl("zea", con)){
+    # the conditions betacar_zea and zea were not measured in parallel with the other conditions
+    specific_zea <- c(parameters[grepl("k", parameters)])
+    trafo[specific_zea] <- paste0(trafo[specific_zea], "*exp(logk_zea)")
+  }
+  if(grepl("betacry",con)){
+     trafo["kb1"] <- "0"
+     trafo["bcar"] <- "0"
+  }
+  if(grepl("betacar", con)){
+    trafo["bcry"] <- "0"
+    trafo["kc1"] <- "0"
+    trafo["kc2"] <- "0"
+    trafo["kc4"] <- "0"
+  }
+  if(con == "betacar_zea"){
+    trafo["bcar"] <- "exp(logbcar_zea)"
+  }
+  if(con == "zea"){
+    trafo["zea"] <- "exp(logzeaini)"
+    trafo["bcry"] <- "0"
+    trafo["bcar"] <- "0"
+    trafo["kc1"] <- "0"
+    trafo["kc2"] <- "0"
+    trafo["kb1"] <- "0"
+    trafo["kb2"] <- "0"
+    trafo["off_ob10"] <- "0"
+  }
+  if(con == "b10"){
+    trafo["b10"] <- "exp(logb10ini)"
+    trafo["bcar"] <- "0"
+    trafo["bcry"] <- "0"
+    trafo["kb1"] <- "0"
+    trafo["kc1"] <- "0"
+    trafo["kc2"] <- "0"
+    trafo["k5"] <- "0"
+    trafo["off_oohb10"] <- "0"
+  }  
+  if(con == "ohb10"){
+    trafo["ohb10"] <- "exp(logohb10ini)"
+    trafo["bcar"] <- "0"
+    trafo["bcry"] <- "0"
+    trafo["kb1"] <- "0"
+    trafo["kc1"] <- "0"
+    trafo["kc2"] <- "0"
+    trafo["k5"] <- "0"
+    trafo["off_ob10"] <- "0"
+  }  
+  if(con == "betacar_b10"){
+    trafo["b10"] <- "exp(logbcar)"
+    trafo["bcar"] <- "0"
+    trafo["bcry"] <- "0"
+    trafo["kc1"] <- "0"
+    trafo["kc2"] <- "0"
+    trafo["kc4"] <- "0"
+  }
+    return(trafo)
+}) 
+names(trafoLTot) <- conditions
+
+p <- NULL
+for (C in conditions) {
+  p <- p + P(trafoLTot[[C]], condition = C)
+}
+
+optionsOde = list(method = "lsoda")#, lrw = 524)
+optionsSens = list(method = "lsodes", rtol = 1e-8, atol = 1e-8)
+x <- Xs(model0, optionsOde = optionsOde, optionsSens = optionsSens)
+
+trafoL <- trafoLTot[conditions]
+
+## Objective Function -------------------------------------------------------
+
+# Initalize parameters 
+outerpars <- getSymbols(do.call(c, trafoL[conditions]))
+set.seed(2)
+# building a weak parameter prior that can be used to ease optimization
+prior <- rep(0, length(outerpars)); names(prior) <- outerpars
+pouter <- rnorm(length(prior), prior, 1); names(pouter) <- outerpars
+cOuter <- constraintL2(mu = prior[names(pouter)], sigma = 10)
+
+# Objective function for trust() 
+obj <- normL2(data, g*x*p) # + cOuter
+
+
+## Howto proceed -------------------------------------------------
+print("now predicting and plotting")
+# Predicting and plotting
+# Data times
+timesD <- unique(sort(c(unlist(sapply(data, function(d) c(0, d$time))))))
+times <- sort(union(timesD,seq(min(timesD), max(timesD), len=101)))
+
+# Fitting
+ini <- pouter
+
+center <- ini
+cat("test obj with ini:",obj(ini)$value)
+if(do.fit){
+  print("start fit")
+  myfit <- trust(obj, ini, rinit=1, rmax=10, iterlim=500,blather=TRUE, conditions = conditions)
+  print(paste("fit",myfit$converged,"with",myfit$iterations,"iterations to",myfit$value))
+  
+  prediction <- (g*x*p)(c(times, seq(130,210, length.out = 10)), myfit$argument, conditions = conditions)
+  
+  print(plotCombined(prediction,  data) + theme_bw())
+  
+  print("chi2 per condition:")
+  cat(dim(do.call(rbind,data))[1], obj(myfit$argument)$value, length(myfit$argument),"total","\n")
+  for(i in conditions){
+    cat(dim(data[[i]])[1],obj(myfit$argument, conditions = i)$value,i,"\n")
+  }
+  
+  plotCombined(prediction,  data, grepl("betacar",condition) & name %in% c("obcar", "ob10")) + xlab("time[min]") + ylab("concentration [nM]")
+}
+
+# Profile likelihood
+if(do.profs){
+  cat("now do profs \n")
+  #profiles.exact  <- do.call(rbind,mclapply(names(myfit$argument), function(n) profile(obj, myfit$argument, n, method = "integrate", limits = c(-5, 5), alpha = 1e-6, stepControl = list(min = .1), conditions = conditions),mc.cores=3))
+  
+  profiles.exact_normal <- profiles.exact # normal logarithmic scale
+  # want to plot the profiles on the non-log scale
+  npp <- attr(profiles.exact, "parameters")
+  snpp <- substr(npp, 4, nchar(npp))
+  profiles.exact[,npp] <- exp(profiles.exact[,npp])
+  colnames(profiles.exact) <- c(colnames(profiles.exact)[1:6],snpp)
+  attr(profiles.exact, "parameters") <- snpp
+  profiles.exact$whichPar <- substr(profiles.exact$whichPar, 4, nchar(profiles.exact$whichPar))
+  print(plotProfile(profiles.exact, maxvalue = 5, grepl("k", name) & !(grepl("k_zea", name))  & mode == "data") + facet_wrap(~name, ncol=1))
+}
