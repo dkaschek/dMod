e20933e97734b0d46790301e36d10e2b78574799
Daniel Lill <daniel.lill@posteo.de>
2018-10-21 23:20:53 +0200

    FEATURE: sbml_import via amici

---
+++ R/SBMLinterface.R
@@ -0,0 +1,44 @@
+#' Import an SMBL model
+#'
+#' Requires AMICI https://github.com/ICB-DCM/AMICI/ and dependencies to be installed on your system
+#' Big thanks go to Daniel Weindl!
+#' 
+#' @param modelpath Path to the sbml file
+#' @param amicipath Path to your amici-python-installation, e.g.: AMICIPATH/python
+#'
+#' @return list of eqnlist, parameters and inits
+#' @export
+#' @importFrom rjson fromJSON 
+#' @importFrom stringr str_replace_all
+import_sbml <- function(modelpath, amicipath = NULL) {
+  
+  importscript <- system.file("code/sbmlAmiciDmod.py", package = "dMod")
+  tmpfile_json <- tempfile()
+  
+  if (!is.null(amicipath))
+    amicipath <- paste0("PYTHONPATH=", amicipath)
+  
+  run_import_script_call <- paste0('bash -c "', amicipath, " python ", importscript, " ", modelpath, " ", tmpfile_json, '"')
+  system(run_import_script_call)
+  json_content <- rjson::fromJSON(file = tmpfile_json)
+  
+  S <- do.call(cbind, json_content[["S"]])
+  S[S==0] <- NA
+  
+  v <- json_content[["v"]]
+  v <- stringr::str_replace_all(v, "\\*\\*", "^")
+  
+  states <- json_content[["stateNames"]]
+  
+  pars <- setNames(json_content[["p"]], json_content[["parameterNames"]])
+  x0 <- setNames(json_content[["x0"]], json_content[["stateNames"]])
+  
+  reactions <- eqnlist(smatrix = S, states = states, rates = v)
+  
+  observables <- json_content[["observables"]]
+  
+  print(names(json_content))
+  
+  out <- list(reactions = reactions, pars = pars, inits = x0, observables = observables)
+  return(out)
+}
---
+++ inst/code/sbmlAmiciDmod.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+#
+# (c) INCOME Hackathon 2018, Bernried, Daniel^2
+#
+
+import sys
+import numpy as np
+import json
+
+try: 
+  import amici.sbml_import
+except:
+  from amici import sbml_import
+  
+def symengineMatrixToNumpy(x, astype='float'):
+    return np.array(x).reshape(x.shape).astype(astype)
+
+def getModelJSON(sbml_file_name):
+
+    importer = amici.sbml_import.SbmlImporter(sbml_file_name, check_validity=False)
+
+
+    observables = amici.sbml_import.assignmentRules2observables(importer.sbml,
+                                                filter_function=lambda variableId:
+    variableId.getId().startswith('observable_') and not
+    variableId.getId().endswith('_sigma'))
+    importer.processSBML()
+    importer.computeModelEquations()
+    
+    S = symengineMatrixToNumpy(importer.stoichiometricMatrix)
+    dataPy = {
+        'S': S.tolist(),
+        'v': [str(x) for x in importer.fluxVector],
+        'p': importer.parameterValues,
+        'stateNames': symengineMatrixToNumpy(importer.symbols['species']['sym'], astype='str').tolist(),
+        'parameterNames': symengineMatrixToNumpy(importer.symbols['parameter']['sym'], astype='str').tolist(),
+        'x0': symengineMatrixToNumpy(importer.speciesInitial, astype='str').tolist(),
+        "observables": observables
+    }
+    data = json.dumps(dataPy)
+
+    return data
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: %s SBML-FILE-NAME [OUTFILE]' % __file__)
+        sys.exit(1)
+
+    sbml_file_name = sys.argv[1]
+    output = getModelJSON(sbml_file_name)
+
+    if len(sys.argv) > 2:
+        outfile = sys.argv[2]
+        with open(outfile, "w") as f:
+            f.write(output)
+    else:
+        print(output)
