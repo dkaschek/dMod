acbb8f79a13e2bcec0c7933251a5d58c9d609026
Daniel Lill <daniel.lill@posteo.de>
2020-02-21 15:24:29 +0100

    the comparisons between old nll versions are satisfying

--- inst/tests/test-nll_wrss_normIndiv.R
+++ inst/tests/test-nll_wrss_normIndiv.R
@@ -72,6 +72,7 @@ a <- mapply(.x = lapply(resResult,function(x) nll(x, ini, TRUE)), .y = readRDS(f
 
 
 # .. 2 error model -----
+# expect Difference, because sigma is present in data!
 mydata <- data
 resResult <- lapply(setNames(nm = conditions), function(cn) {
   err <- e(prediction[[cn]], ini)
@@ -110,14 +111,40 @@ a <- mapply(.x = lapply(resResult,function(x) nll(x, ini, TRUE)), .y = readRDS(f
 
 
 # .. 3 error model, delete sigma first -----
+# should be the same again
 mydata <- as.datalist(lapply(data, function(x) {x$sigma = NA;x}))
 resResult <- lapply(setNames(nm = conditions), function(cn) {
   err <- e(prediction[[cn]], ini)
   res(mydata[[cn]], prediction[[cn]], err[[1]])
 })
-identical(mydata, readRDS(file.path("etc/example_CCD4/expectations", "031-data.rds")))
-identical(resResult, readRDS(file.path("etc/example_CCD4/expectations", "032-resResultErrpars.rds")))
-identical(lapply(resResult,nll), readRDS(file.path("etc/example_CCD4/expectations", "033-wrss.rds")))
+# [x] Data is same
+identical(mydata, readRDS(file.path("etc/example_CCD4/expectations","031-data.rds")))
+
+mapply(.x = resResult, .y = readRDS(file.path("etc/example_CCD4/expectations","032-resResultErrpars.rds")), FUN = function(.x,.y) {
+  merge(.x,.y, all = TRUE)
+  # print("new");print(.x);print("old");print(.y)
+}, SIMPLIFY = F)
+
+a <- mapply(.x = lapply(resResult,function(x) nll(x, ini, TRUE)), .y = readRDS(file.path("etc/example_CCD4/expectations","033-wrss.rds")),FUN = function(.x,.y) {
+  # cat("\n\nnew\n");print(.x);cat("\n\nold\n");print(.y)
+  
+  cat(crayon::green("values\n"))
+  print(.x$value)
+  print(.y$value)
+  
+  cat(crayon::green("gradient lengths\n"))
+  print(length(.x$gradient))
+  print(length(.y$gradient))
+  
+  cat(crayon::green("gradients\n"))
+  print(compare_named_numeric(.x$gradient, .y$gradient))
+  
+  
+  # cat(crayon::green("hessians\n"))
+  # print(compare_hessians(.x$hessian, .y$hessian))
+  a <- NULL
+})
+
 
 # .. 4 Data with LLOQ, sigma from data -----
 mydata <- as.datalist(lapply(data, function(x) {x$lloq = median(x$value);x}))
@@ -126,19 +153,68 @@ resResult <- lapply(setNames(nm = conditions), function(cn) {
   err <- NULL
   res(mydata[[cn]], prediction[[cn]], err[[1]])
 })
+# [x] Data is same
 identical(mydata, readRDS(file.path("etc/example_CCD4/expectations","041-data.rds")))
-identical(resResult, readRDS(file.path("etc/example_CCD4/expectations","042-resResultErrpars.rds")))
-identical(lapply(resResult,wrss), readRDS(file.path("etc/example_CCD4/expectations","043-wrss.rds")))
+
+mapply(.x = resResult, .y = readRDS(file.path("etc/example_CCD4/expectations","042-resResultErrpars.rds")), FUN = function(.x,.y) {
+  merge(.x,.y, all = TRUE)
+  # print("new");print(.x);print("old");print(.y)
+}, SIMPLIFY = F)
+
+a <- mapply(.x = lapply(resResult,function(x) nll(x, ini, TRUE)), .y = readRDS(file.path("etc/example_CCD4/expectations","043-wrss.rds")),FUN = function(.x,.y) {
+  # cat("\n\nnew\n");print(.x);cat("\n\nold\n");print(.y)
+  
+  cat(crayon::green("values\n"))
+  print(.x$value)
+  print(.y$value)
+  
+  cat(crayon::green("gradient lengths\n"))
+  print(length(.x$gradient))
+  print(length(.y$gradient))
+  
+  cat(crayon::green("gradients\n"))
+  print(compare_named_numeric(.x$gradient, .y$gradient))
+  
+  
+  # cat(crayon::green("hessians\n"))
+  # print(compare_hessians(.x$hessian, .y$hessian))
+  a <- NULL
+})
 
 # .. 5 Data with LLOQ, sigma from errorModel -----
+# expect difference
 mydata <- as.datalist(lapply(data, function(x) {x$lloq = median(x$value);x}))
 resResult <- lapply(setNames(nm = conditions), function(cn) {
   err <- e(prediction[[cn]], ini)
   res(mydata[[cn]], prediction[[cn]], err[[1]])
 })
+# [x] Data is same
 identical(mydata, readRDS(file.path("etc/example_CCD4/expectations","051-data.rds")))
-identical(resResult, readRDS(file.path("etc/example_CCD4/expectations","052-resResultErrpars.rds")))
-identical(lapply(resResult,nll), readRDS(file.path("etc/example_CCD4/expectations","053-wrss.rds")))
+
+mapply(.x = resResult, .y = readRDS(file.path("etc/example_CCD4/expectations","052-resResultErrpars.rds")), FUN = function(.x,.y) {
+  merge(.x,.y, all = TRUE)
+  # print("new");print(.x);print("old");print(.y)
+}, SIMPLIFY = F)
+
+a <- mapply(.x = lapply(resResult,function(x) nll(x, ini, TRUE)), .y = readRDS(file.path("etc/example_CCD4/expectations","053-wrss.rds")),FUN = function(.x,.y) {
+  # cat("\n\nnew\n");print(.x);cat("\n\nold\n");print(.y)
+  
+  cat(crayon::green("values\n"))
+  print(.x$value)
+  print(.y$value)
+  
+  cat(crayon::green("gradient lengths\n"))
+  print(length(.x$gradient))
+  print(length(.y$gradient))
+  
+  cat(crayon::green("gradients\n"))
+  print(compare_named_numeric(.x$gradient, .y$gradient))
+  
+  
+  # cat(crayon::green("hessians\n"))
+  # print(compare_hessians(.x$hessian, .y$hessian))
+  a <- NULL
+})
 
 
 # .. 6 Data with LLOQ, delete sigma first -----
@@ -147,11 +223,38 @@ resResult <- lapply(setNames(nm = conditions), function(cn) {
   err <- e(prediction[[cn]], ini)
   res(mydata[[cn]], prediction[[cn]], err[[1]])
 })
+# [x] Data is same
 identical(mydata, readRDS(file.path("etc/example_CCD4/expectations","061-data.rds")))
-identical(resResult, readRDS(file.path("etc/example_CCD4/expectations","062-resResultErrpars.rds")))
-identical(lapply(resResult,nll), readRDS(file.path("etc/example_CCD4/expectations","063-wrss.rds")))
+
+mapply(.x = resResult, .y = readRDS(file.path("etc/example_CCD4/expectations","062-resResultErrpars.rds")), FUN = function(.x,.y) {
+  merge(.x,.y, all = TRUE)
+  # print("new");print(.x);print("old");print(.y)
+}, SIMPLIFY = F)
+
+a <- mapply(.x = lapply(resResult,function(x) nll(x, ini, TRUE)), .y = readRDS(file.path("etc/example_CCD4/expectations","063-wrss.rds")),FUN = function(.x,.y) {
+  # cat("\n\nnew\n");print(.x);cat("\n\nold\n");print(.y)
+  
+  cat(crayon::green("values\n"))
+  print(.x$value)
+  print(.y$value)
+  
+  cat(crayon::green("gradient lengths\n"))
+  print(length(.x$gradient))
+  print(length(.y$gradient))
+  
+  cat(crayon::green("gradients\n"))
+  print(compare_named_numeric(.x$gradient, .y$gradient))
+  
+  
+  # cat(crayon::green("hessians\n"))
+  # print(compare_hessians(.x$hessian, .y$hessian))
+  a <- NULL
+})
 
 
+# -------------------------------------------------------------------------#
+# SectionTitle ----
+# -------------------------------------------------------------------------#
 for (f in list.files(pattern = "\\.(c|o|so)$")) unlink(f)
 #-!End example code
 
