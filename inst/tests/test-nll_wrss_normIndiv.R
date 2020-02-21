a3f07ad1fc7dd88ae2a5abb629acb57fa0bdd2f5
Daniel Lill <daniel.lill@posteo.de>
2020-02-21 10:23:19 +0100

    preliminary unit test

---
+++ inst/tests/test-nll_wrss_normIndiv.R
@@ -0,0 +1,77 @@
+# context("nll_wrss")
+# test_that("IQR-nll", {
+
+#-!Start example code
+source("etc/example_CCD4/001-setup.R")
+
+expect_identical(obj(ini), readRDS(file.path("expectations","001-objResult.rds")))
+# .. 1 normal -----
+mydata <- data
+resResult <- lapply(setNames(nm = conditions), function(cn) {
+  res(mydata[[cn]], prediction[[cn]])
+})
+expect_identical(mydata, readRDS(file.path("expectations","011-data.rds")))
+expect_identical(resResult, readRDS(file.path("expectations","012-resResult.rds")))
+expect_identical(lapply(resResult,wrss), readRDS(file.path("expectations","013-wrss.rds")))
+
+
+# .. 2 error model -----
+mydata <- data
+resResult <- lapply(setNames(nm = conditions), function(cn) {
+  err <- e(prediction[[cn]], ini)
+  res(mydata[[cn]], prediction[[cn]], err[[1]])
+})
+expect_identical(mydata, readRDS(file.path("expectations","021-data.rds")))
+expect_identical(resResult, readRDS(file.path("expectations","022-resResultErrpars.rds")))
+expect_identical(lapply(resResult,nll), readRDS(file.path("expectations","023-wrss.rds")))
+
+# .. 3 error model, delete sigma first -----
+mydata <- as.datalist(lapply(data, function(x) {x$sigma = NA;x}))
+resResult <- lapply(setNames(nm = conditions), function(cn) {
+  err <- e(prediction[[cn]], ini)
+  res(mydata[[cn]], prediction[[cn]], err[[1]])
+})
+expect_identical(mydata, readRDS(file.path("expectations", "031-data.rds")))
+expect_identical(resResult, readRDS(file.path("expectations", "032-resResultErrpars.rds")))
+expect_identical(lapply(resResult,nll), readRDS(file.path("expectations", "033-wrss.rds")))
+
+# .. 4 Data with LLOQ, sigma from data -----
+mydata <- as.datalist(lapply(data, function(x) {x$lloq = median(x$value);x}))
+resResult <- lapply(setNames(nm = conditions), function(cn) {
+  # err <- e(prediction[[cn]], ini)
+  err <- NULL
+  res(mydata[[cn]], prediction[[cn]], err[[1]])
+})
+expect_identical(mydata, readRDS(file.path("expectations","041-data.rds")))
+expect_identical(resResult, readRDS(file.path("expectations","042-resResultErrpars.rds")))
+expect_identical(lapply(resResult,wrss), readRDS(file.path("expectations","043-wrss.rds")))
+
+# .. 5 Data with LLOQ, sigma from errorModel -----
+mydata <- as.datalist(lapply(data, function(x) {x$lloq = median(x$value);x}))
+resResult <- lapply(setNames(nm = conditions), function(cn) {
+  err <- e(prediction[[cn]], ini)
+  res(mydata[[cn]], prediction[[cn]], err[[1]])
+})
+expect_identical(mydata, readRDS(file.path("expectations","051-data.rds")))
+expect_identical(resResult, readRDS(file.path("expectations","052-resResultErrpars.rds")))
+expect_identical(lapply(resResult,nll), readRDS(file.path("expectations","053-wrss.rds")))
+
+
+# .. 6 Data with LLOQ, delete sigma first -----
+mydata <- as.datalist(lapply(data, function(x) {x$lloq = median(x$value);x$sigma = NA;x}))
+resResult <- lapply(setNames(nm = conditions), function(cn) {
+  err <- e(prediction[[cn]], ini)
+  res(mydata[[cn]], prediction[[cn]], err[[1]])
+})
+expect_identical(mydata, readRDS(file.path("expectations","061-data.rds")))
+expect_identical(resResult, readRDS(file.path("expectations","062-resResultErrpars.rds")))
+expect_identical(lapply(resResult,nll), readRDS(file.path("expectations","063-wrss.rds")))
+
+
+for (f in list.files(pattern = "\\.(c|o|so)$")) unlink(f)
+#-!End example code
+
+
+# Define your expectations here
+
+#})
