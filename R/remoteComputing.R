80d189b54e712bb911042fa0a4adf4220ef2700b
marcusrosenblatt <marcus.rosenblatt@gmail.com>
2020-09-17 19:20:23 +0200

    Add: Run R expression on the bw-cluster via sshpass and update of documentation

--- R/remoteComputing.R
+++ R/remoteComputing.R
@@ -281,6 +281,8 @@ runbg <- function(..., machine = "localhost", filename = NULL, input = ls(.Globa
 #' into an R data file and copied to the remove machine.
 #' @param compile Logical. If \code{TRUE}, C files are copied and compiled on the remote machine.
 #' Otherwise, the .so files are copied.
+#' @param recover Logical, If \cde{TRUE}, the scripts will not be started again.
+#' Can be used to get back the check and get functions for an already started process, e.g. after local session has aborted.
 #' @return List of functions \code{check()}, \code{get()} and \code{purge()}. 
 #' \code{check()} checks, if the result is ready.
 #' \code{get()} copies the result file
@@ -459,6 +461,212 @@ runbg_bwfor <- function(..., machine, filename = NULL, nodes = 1, cores = 1, wal
   
 }
 
+
+#' Run an R expression on the bwForCluster via sshpass
+#' 
+#' @description Generate an R code of the expression that is copied via \code{scp}
+#' to the bwForCluster. Then collect the results. ssh-key not needed. Password can be provided via an additional argument.
+#' sshpass needs to be installed on your local machine.
+#' @details \code{runbg()} generates a workspace from the \code{input} argument
+#' and copies the workspace and all C files or .so files to the remote machines via
+#' \code{scp}. This will only work if *an ssh-key had been generated and added
+#' to the authorized keys on the remote machine*. The
+#' code snippet, i.e. the \code{...} argument, can include several intermediate results
+#' but only the last call which is not redirected into a variable is returned via the
+#' variable \code{.runbgOutput}, see example below.
+#' @param ... Some R code
+#' @param machine e.g. \code{fr_dk846@@bwfor.cluster.uni-mannheim.de}
+#' @param filename Character, defining the filename of the temporary file. Random
+#' file name ist chosen if NULL.
+#' @param nodes Number of nodes, e.g. 10
+#' @param cores Number of cores, e.g. 16
+#' @param walltime estimated runtime in the format \code{hh:mm:ss}, e.g. \code{01:30:00}.
+#' Jobs with a walltime up to 30 min are sent to a quick queue. When the walltime
+#' is exceeded, all jobs are automatically killed by the queue.
+#' @param input Character vector, the objects in the workspace that are stored
+#' into an R data file and copied to the remove machine.
+#' @param compile Logical. If \code{TRUE}, C files are copied and compiled on the remote machine.
+#' Otherwise, the .so files are copied.
+#' @param recover Logical, If \cde{TRUE}, the scripts will not be started again.
+#' Can be used to get back the check and get functions for an already started process, e.g. after local session has aborted.
+#' @param password Your ssh password in plain text (yes, no joke unfortunately), the password is handed over to sshpass for automatic login on the cluster.
+#' @return List of functions \code{check()}, \code{get()} and \code{purge()}. 
+#' \code{check()} checks, if the result is ready.
+#' \code{get()} copies the result file
+#' to the working directory and loads it into the workspace as an object called \code{.runbgOutput}. 
+#' This object is a list named according to the machines that contains the results returned by each
+#' machine.
+#' \code{purge()} deletes the temporary folder
+#' from the working directory and the remote machines.
+#' @examples
+#' \dontrun{
+#' out_job1 <- runbg({
+#'    mstrust(obj, center, fits = 10, cores = 2)
+#'  }, 
+#'  machine = "bwfor", nodes = 2, cores = "2:best", 
+#'  walltime = "00:01:00", 
+#'  filename = "job1")
+#' out_job1$check()          
+#' out_job1$get()
+#' out_job1$purge()
+#' result <- .runbgOutput
+#' print(result)
+#' }
+#' 
+#' @export
+runbg_bwfor_sshpass <- function(..., machine, filename = NULL, nodes = 1, cores = 1, walltime = "01:00:00", input = ls(.GlobalEnv), compile = TRUE, recover = F, password="'begin__end'") {
+  
+  
+  expr <- as.expression(substitute(...))
+  
+  # Set file name
+  if (is.null(filename))
+    filename <- paste0("tmp_", paste(sample(c(0:9, letters), 5, replace = TRUE), collapse = ""))
+  
+  filename0 <- filename
+  filename <- paste(filename, 1:nodes, sep = "_")
+  
+  # Initialize output
+  out <- structure(vector("list", 3), names = c("check", "get", "purge"))
+  out[[1]] <- function() {
+    
+    check.out <- length(suppressWarnings(
+      system(paste0("sshpass -p ",password, " ssh ", machine, " ls ", filename0, "_folder/ | grep result.RData"), 
+             intern = TRUE)))
+    
+    if (check.out == nodes) {
+      cat("Result is ready!\n")
+      return(TRUE)
+    }
+    else if (check.out  < nodes) {
+      cat("Result from", check.out, "out of", nodes, "nodes are ready.")
+      return(FALSE)
+    }
+    
+    
+  }
+  
+  out[[2]] <- function() {
+    
+    result <- structure(vector(mode = "list", length = nodes))
+    system(paste0("sshpass -p ",password, " scp ", machine, ":", filename0, "_folder/*", "_result.RData ./"), ignore.stdout = TRUE, ignore.stderr = TRUE)
+    for (m in 1:nodes) {
+      .runbgOutput <- NULL
+      check <- try(load(file = paste0(filename[m], "_result.RData")), silent = TRUE) 
+      if (!inherits("try-error", check)) result[[m]] <- .runbgOutput
+    }
+    
+    .GlobalEnv$.runbgOutput <- result
+    
+  }
+  
+  out[[3]] <- function() {
+    
+    system(paste0("rm ", filename0, "*"), wait = TRUE)
+    system(paste0("sshpass -p ",password, " ssh ", machine, " rm -r ", filename0, "*"), wait = TRUE)
+    
+  }
+  if(recover) return(out)
+  
+  # Save current workspace
+  save(list = input, file = paste0(filename0, ".RData"), envir = .GlobalEnv)
+  
+  # Get loaded packages
+  pack <- sapply(strsplit(search(), "package:", fixed = TRUE), function(v) v[2])
+  pack <- pack[!is.na(pack)]
+  pack <- paste(paste0("try(library(", pack, "))"), collapse = "\n")
+  
+  # Define outputs
+  output <- ".runbgOutput"
+  
+  
+  
+  # Write program into character
+  program <- lapply(1:nodes, function(m) {
+    paste(
+      pack,
+      paste0("setwd('~/", filename0, "_folder')"),
+      "rm(list = ls())",
+      "library(doParallel)",
+      "procs <- as.numeric(Sys.getenv('MOAB_PROCCOUNT'))",
+      "registerDoParallel(cores=procs)",
+      paste0("load('", filename0, ".RData')"),
+      "files <- list.files(pattern = '.so')",
+      "for (f in files) dyn.load(f)",
+      paste0(".node <- ", m),
+      paste0(".runbgOutput <- try(", as.character(expr), ")"),
+      
+      paste0("save(", output ,", file = '", filename[m], "_result.RData')"),
+      sep = "\n"
+    )
+  })
+  
+  # Write program code into file
+  for (m in 1:nodes) cat(program[[m]], file = paste0(filename[m], ".R"))
+  
+  # Write job file to be called by msub
+  job <- lapply(1:nodes, function(m) {
+    paste(
+      "#!/bin/sh", 
+      "########## Begin MOAB/Slurm header ##########",
+      "#",
+      "# Give job a reasonable name",
+      paste0("#MOAB -N ", filename[m]),
+      "#",
+      "# Request number of nodes and CPU cores per node for job",
+      paste0("#MOAB -l nodes=1:ppn=", cores),
+      "#",
+      "# Estimated wallclock time for job",
+      paste0("#MOAB -l walltime=", walltime),
+      "#",
+      "# Write standard output and errors in same file",
+      "#MOAB -j oe ",
+      "#",
+      "########### End MOAB header ##########",
+      "",
+      "# Setup R Environment",
+      "module load math/R",
+      "export OPENBLAS_NUM_THREADS=1",
+      "# Start program",
+      paste0("R CMD BATCH --no-save --no-restore --slave ", filename0, "_folder/", filename[m], ".R"),
+      sep = "\n"
+    )
+  })
+  
+  # Write job file to file
+  for (m in 1:nodes) cat(job[[m]], file = paste0(filename[m], ".moab"))
+  
+  # Copy files to temporal folder
+  system(paste0("sshpass -p ",password, " ssh ", machine, " mkdir ", filename0, "_folder/"), ignore.stdout = TRUE, ignore.stderr = TRUE)
+  system(paste0("sshpass -p ",password, " ssh ", machine, " rm ", filename0, "_folder/*"), ignore.stdout = TRUE, ignore.stderr = TRUE)
+  system(paste0("sshpass -p ",password, " scp ", getwd(), "/", filename0, ".RData* ", machine, ":", filename0, "_folder/"))
+  system(paste0("sshpass -p ",password, " scp ", getwd(), "/", filename0, "*.R* ", machine, ":", filename0, "_folder/"))
+  system(paste0("sshpass -p ",password, " scp ", getwd(), "/", filename0, "*.moab ", machine, ":"))
+  if (compile) {
+    
+    sourcefiles <- paste(
+      paste0(
+        filename0, "_folder/", 
+        c(list.files(pattern = glob2rx("*.c")), list.files(pattern = glob2rx("*.cpp")))
+      ), 
+      collapse = " "
+    )
+    
+    system(paste0("sshpass -p ",password, " scp ", getwd(), "/*.c ", getwd(), "/*.cpp ", machine, ":", filename0, "_folder/"))
+    system(paste0("sshpass -p ",password, " ssh ", machine, " 'module load math/R; R CMD SHLIB ", sourcefiles, " -o ", filename0, "_folder/", filename0, ".so'"))
+  } else {
+    system(paste0("sshpass -p ",password, " scp ", getwd(), "/*.so ", machine, ":", filename0, "_folder/"))
+  }
+  
+  
+  # Run in background
+  for (m in 1:nodes) system(paste0("sshpass -p ",password, " ssh ", machine, " msub ", filename[m], ".moab"), intern = FALSE)
+  
+  
+  return(out)
+  
+  
+}
 
 
 
