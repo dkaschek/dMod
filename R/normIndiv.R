3ab183efea69142bf84182aa36dee6db744701f5
Svenja Kemmer <svenja.kemmer@fdm.uni-freiburg.de>
2022-01-31 16:06:53 +0100

    adjust getParGrids to normIndiv

--- R/normIndiv.R
+++ R/normIndiv.R
@@ -858,3 +858,126 @@ check_and_sanitize_prediction <- function(prediction, data, cn, FLAGNaNInfwarnin
   }
   prediction
 }
+
+#' Get est.grid and fixed.grid
+#'
+#' @param mytrafo base trafo
+#' @param mytrafoL condition specific branched trafo list
+#' @param mycondition.grid condition.grid
+#' @param SS_pars parameters determined by steady state
+#'
+#' @return
+#' @export
+#' @author Svenja Kemmer
+#'
+getParGrids <- function(mytrafo, mytrafoL, mycondition.grid, SS_pars = NULL){
+  
+  # .. condition.grid -----
+  
+  myconditions <- mycondition.grid$condition
+  rownames(mycondition.grid) <- myconditions
+  
+  # .. fixed.grid - all conditions fixed -----
+  
+  # select all trafo elements containing numbers
+  fixed_trafo <- mytrafo[suppressWarnings(which(!(mytrafo %>% as.numeric()) %>% is.na()))] %>% as.eqnvec()
+  fixed_df <- data.frame(as.list(fixed_trafo), stringsAsFactors=FALSE) 
+  
+  fixed_df2 <- fixed_df %>% rbind(fixed_df[rep(1, (length(myconditions)-1)), ]) %>% 
+    cbind(condition = myconditions, ID = 1:length(myconditions)) %>% 
+    select(ID, condition, everything()) 
+  
+  fixedpars <- names(fixed_trafo)
+  
+  # .. est.grid -----
+  
+  estpars <- names(mytrafo)[!names(mytrafo) %in% c(names(fixed_trafo), SS_pars)]
+  est_trafo <- mytrafo[estpars]
+  trafo_counterparts <- getSymbols(mytrafo)
+  est_df <- NULL
+  
+  for(cond in myconditions){
+    conditrafo <- mytrafoL[[cond]][estpars]
+    if(any(c(str_detect(conditrafo, "exp\\("),str_detect(conditrafo, "10\\^\\(")))) conditrafo <- gsub("exp\\(", "", conditrafo) %>% gsub("10\\^\\(", "", .) %>% gsub("\\(", "", .) %>% gsub("\\)", "", .)
+    if(any(c(str_detect(mytrafo, "exp\\("),str_detect(mytrafo, "10\\^\\(")))) mytrafo <- gsub("exp\\(", "", mytrafo) %>% gsub("10\\^\\(", "", .) %>%gsub("\\(", "", .) %>% gsub("\\)", "", .)
+    
+    # check for mathematical parameter trafos
+    myoperations <- c("/|\\+|\\*")
+    if(any(grepl(myoperations, conditrafo))){
+      myreplpars <- grep(myoperations, conditrafo, value = TRUE)
+      myorigpars <- grep(myoperations, mytrafo, value = TRUE)
+      
+      addpars <- NULL
+      for(i in names(myreplpars)){
+        myreplpar <- conditrafo[i]
+        myorigpar <- mytrafo[i]
+        parsorig <- strsplit(myorigpar, split = myoperations)[[1]]
+        parsrepl <- strsplit(myreplpar, split = myoperations)[[1]]
+        names(parsrepl) <- parsorig
+        # check weather pars are already present in addpars
+        for(j in names(parsrepl)){
+          if(!(j %in% names(addpars))) addpars <- c(addpars, parsrepl[j])
+        }  
+        # remove numbers from original trafo
+        addpars <- addpars[!grepl("^-?[[:digit:]]", names(addpars))]
+      }
+      
+      excludenames <- intersect(c(names(addpars), names(myreplpars)), names(conditrafo))
+      # substitute corresponding pars
+      reduced_trafo <- subset(conditrafo, !names(conditrafo) %in% excludenames)
+      conditrafo <- c(reduced_trafo, addpars)
+    }
+    
+    # set all numbers/numerics = NA
+    trafo.mod <- suppressWarnings(do.call(c, lapply(conditrafo, function(x) {fgh <- as.numeric(x); 
+    if(is.na(fgh)){x} else NA})))
+    
+    mypars <- names(trafo.mod[!is.na(trafo.mod)])
+    
+    # filter condition specific est pars
+    specifictrafo <- trafo.mod[mypars]
+    # filter general est pars
+    generaltrafo <- trafo.mod[mypars] %>% names()
+    # filter general fixed pars
+    numbertrafo <- setdiff(names(trafo.mod), mypars)
+    # filter specific fixed numbers with general fixed pars as names
+    numbers <- conditrafo[numbertrafo] %>% unclass()
+    
+    est_row <- data.frame(id = c(generaltrafo, names(numbers)), var = c(specifictrafo, numbers)) %>% data.table::transpose()
+    names(est_row) <- est_row[1,]
+    est_row <- est_row[-1,] %>% mutate(condition = cond)
+    
+    est_df <- rbind(est_df, est_row)
+  }
+  
+  # .. fixed.grid - partly fixed conditions -----
+  
+  # filter fixedpars from est.grid 
+  partly_fixed_df <- est_df %>% select_if(function(x) any(grepl("^-?[[:digit:]]",x))) 
+  # assign NA to est pars
+  partly_fixed_df <- sapply(partly_fixed_df, function(x) {
+    sapply(x, function(z){
+      if(!grepl("^-?[[:digit:]]", z)) z <- NA
+      else z <- as.numeric(z)
+    })
+  })
+  
+  # append to fixed.grid
+  fixed.grid <- fixed_df2 %>% cbind(partly_fixed_df %>% as.data.frame(stringsAsFactors = F)) %>% as_tibble() %>% as.data.table()
+  
+  numcols <- setdiff(names(fixed.grid), c("ID", "condition"))
+  fixed.grid[, (numcols) :=lapply(.SD, function(x) as.numeric(x)), .SDcols=numcols]
+  
+  # assign NA to fixed pars
+  est_df <- sapply(est_df, function(x) {
+    sapply(x, function(z){
+      if(grepl("^-?[[:digit:]]", z)) z <- NA
+      else z <- z
+    })
+  })
+  
+  est.grid  <- est_df %>% as.data.frame(stringsAsFactors = F) %>% mutate(ID = 1:length(myconditions)) %>% 
+    select(ID, condition, everything()) %>% as_tibble() %>% as.data.table()
+  
+  list(est.grid, fixed.grid)
+}
